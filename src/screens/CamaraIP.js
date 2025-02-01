cb8d5fe593e1860d890cee93d8a74646a5ef4fec
4ndr3$ <andres30092000@hotmail.com>
2025-02-01 11:21:20 -0500

    Arreglo de camara ip con gemini

--- src/screens/CamaraIP.js
+++ src/screens/CamaraIP.js
@@ -3,12 +3,10 @@ import { WebView } from 'react-native-webview';
 
 function CamaraIP() {
     return (
-        <View>
             <WebView
-                source={{ uri: 'http://10.167.174.104:8080/video' }}
+                source={{ uri: 'http://dispo:dispo@10.167.174.104:8080/video' }}
                 style={{ marginTop: 20 }}
             />
-        </View>
     )
 }
 
