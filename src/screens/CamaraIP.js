6a6aee310eca12f589e23c69af082bbe5e136a16
rfromerol1 <rogerromero921@gmail.com>
2025-02-10 11:18:57 -0500

    agregar camara IP
    código para manejo de cámara ip

---
+++ src/screens/CamaraIP.js
@@ -0,0 +1,36 @@
+import { WebView } from 'react-native-webview';
+import { StyleSheet, View, Text } from 'react-native';
+
+function CamaraIP() {
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Cámara Activada</Text>
+      <View>
+        <Text>IP Webcam</Text>
+      </View>
+      <WebView
+        //source={{ uri: 'http://dispo:dispo@10.216.220.111:8080' }}
+        source={{ uri: 'http://62.131.207.209:8080/' }}
+        style={styles.webview}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 10,
+  },
+  webview: {
+    flex: 1,
+  }
+});
+
+export default CamaraIP;
