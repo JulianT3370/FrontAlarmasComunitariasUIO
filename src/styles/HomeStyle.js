e85c939852517e6e4e752551273a6dd10228b2c8
rfromerol1 <rogerromero921@gmail.com>
2025-02-11 10:32:49 -0500

    HomeStyle
    separación de estilos

---
+++ src/styles/HomeStyle.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const HomeStyles = StyleSheet.create({
+    view: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+    },
+    button: {
+        borderRadius: 10,
+        padding: 15,
+        backgroundColor: "blue",
+        width: "50%",
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "white",
+    },
+});
